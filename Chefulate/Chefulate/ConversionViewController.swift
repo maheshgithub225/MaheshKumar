aea0edd29e18d99841435ad351ba5b685a8e227b
Kevin Crosby <Jayme@jayme-crosbys-macbook-pro.local>
2016-11-08 01:27:16 -0600

    Added quite a bit. added new recipe screen, conversion screen, more ingredients screen, main menu, edited images and files here and there. added segue ways, multiple uipickers, textfields, labels, buttons, and coded the uipickers to show measurements. some more was added that may not have been listed

--- Chefulate/Chefulate/ConversionViewController.swift
+++ Chefulate/Chefulate/ConversionViewController.swift
@@ -4,15 +4,47 @@
 //
 //  Created by Bryan Reynolds on 10/17/16.
 //  Copyright © 2016 Johnathan Taylor Sutton. All rights reserved.
+/*
+ pounds
+ ounces
+ cups
+ teaspoons
+ tablespoons
+ 
+ 
+ 
+ */
 //
 
 import UIKit
 
-class ConversionViewController: UIViewController {
-
+class ConversionViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate,UITextFieldDelegate{
+    @IBOutlet weak var myPicker: UIPickerView!
+    
+    @IBOutlet weak var myPickertwo: UIPickerView!
+    @IBOutlet weak var myPickerthree: UIPickerView!
+    
+    @IBOutlet weak var myPickerfour: UIPickerView!
+    @IBOutlet weak var myPickerfive: UIPickerView!
+    
+    @IBOutlet weak var myPickersix: UIPickerView!
+let pickerData = ["pounds","ounces","tbsp","tsp","cups"]
+    let pickerDatatwo = ["pounds","ounces","tbsp","tsp","cups"]
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        myPicker.dataSource = self
+        myPicker.delegate = self
+        myPickertwo.dataSource = self
+        myPickertwo.delegate = self
+        myPickerthree.dataSource = self
+        myPickerthree.delegate = self
+        myPickerfour.dataSource = self
+        myPickerfour.delegate = self
+        myPickerfive.dataSource = self
+        myPickerfive.delegate = self
+        myPickersix.dataSource = self
+        myPickersix.delegate = self
+        
         // Do any additional setup after loading the view.
     }
 
@@ -20,7 +52,50 @@ class ConversionViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        if pickerView==myPicker{
+        return 1
+        }
+        else if pickerView == myPickertwo{
+        return 1
+        }
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == myPicker{
+     return 5
+        }
+        if pickerView == myPickertwo{
+        return 5
+        }
+        return 5
+        
+        
+        
+        
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == myPicker{
+            print("aaaaaaa")
+        return pickerData[row]
+        }
+        else if pickerView == myPickertwo{
+            print("BBBBBBB")
+        return pickerDatatwo[row]
+        }
+        return pickerDatatwo[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        
+    }
+    @IBAction func myUnwindAction(segue: UIStoryboardSegue){
+        if let source = segue.source as? add_more_ingredients{
+            //count = source.count
+            //print("unwind count \(count)")
+        }
+       
+    }
 
     /*
     // MARK: - Navigation
---
+++ Chefulate/add_more_ingredients.swift
@@ -0,0 +1,35 @@
+//
+//  add_more_ingredients.swift
+//  Chefulate
+//
+//  Created by Jayme Crosby on 11/8/16.
+//  Copyright © 2016 Johnathan Taylor Sutton. All rights reserved.
+//
+
+import UIKit
+
+class add_more_ingredients: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
