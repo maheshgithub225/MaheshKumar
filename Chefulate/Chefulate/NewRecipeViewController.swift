d7eb7394bb1f1c682deca3da55db623d130194df
Mahesh Kumar Kankipati <maheshkumarkankipati@maheshs-mbp.osustudent.okstate.edu>
2016-11-14 15:27:48 -0600

    Adding New Recipe Controller

--- Chefulate/Chefulate/NewRecipeViewController.swift
+++ Chefulate/Chefulate/NewRecipeViewController.swift
@@ -23,7 +23,41 @@ class NewRecipeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    @IBAction func submitRecipe(_ sender: AnyObject) {
+        addRecipeData()
+    }
+    func addRecipeData(){
+        let NewRecipeTitle = RecipeTitle.text!
+        let ing1 = Ingredient1.text!
+        
+        let url = URL(string: "https://cs.okstate.edu/~jtsutto/services.php/0/\(NewRecipeTitle)/\(ing1)")!
+        print("URL: \(url)")
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        
+        let task = session.dataTask(with: url){(data,response,error)in
+            guard error == nil else{
+                print("Error in session call: \(error)")
+                return
+            }
+            guard let result = data else {
+                print("No data reveived")
+                return
+            }
+            do {
+                let json = try JSONSerialization.jsonObject(with: result, options: .allowFragments) as? NSDictionary
+                print("JSON data returned : \(json)")
+            }catch {
+                print("Error Serializing JSON data : \(error)")
+            }
+        }
+        
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let vc : UIViewController = mainStoryboard.instantiateViewController(withIdentifier: "MyRecipesViewController") as UIViewController
+        self.present(vc, animated: true, completion: nil)
+        task.resume()
+    }
+
 
     /*
     // MARK: - Navigation
