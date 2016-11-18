dc93ae39689dcd66e74fbd8edb6bfec77c6b8bce
Mahesh Kumar Kankipati <maheshkumarkankipati@maheshs-mbp.osustudent.okstate.edu>
2016-11-18 11:50:43 -0600

    Design Changes

--- Chefulate/Chefulate/IngredientsTableViewController.swift
+++ Chefulate/Chefulate/IngredientsTableViewController.swift
@@ -16,7 +16,7 @@ class IngredientsTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.navigationItem.leftBarButtonItem?.isEnabled = true
         getIngredients()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
