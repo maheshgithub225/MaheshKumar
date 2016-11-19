8baeac8500a6f672fc5eb9ca5feb334f14997e4e
Johnathan Taylor Sutton <jtsutto@mac09-cs.ms-2345.okstate.edu>
2016-11-19 16:52:52 -0600

    re-added labels to my cells

---
+++ Chefulate/Chefulate/SearchRecipeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchRecipeTableViewCell.swift
+//  Chefulate
+//
+//  Created by Johnathan Taylor Sutton on 11/19/16.
+//  Copyright © 2016 Johnathan Taylor Sutton. All rights reserved.
+//
+
+import UIKit
+
+class SearchRecipeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
