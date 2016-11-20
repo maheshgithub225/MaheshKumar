6ce52560a671205b3c310556e58cb00028ce6b83
Johnathan Taylor Sutton <jtsutto@mac09-cs.ms-2345.okstate.edu>
2016-11-20 15:20:49 -0600

    added files

---
+++ Chefulate/Chefulate/NewInstructionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewInstructionViewController.swift
+//  Chefulate
+//
+//  Created by Johnathan Taylor Sutton on 11/20/16.
+//  Copyright © 2016 Johnathan Taylor Sutton. All rights reserved.
+//
+
+import UIKit
+
+class NewInstructionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
