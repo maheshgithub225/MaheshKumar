af0353e5b80d9483ac9dc134a69ac4469af9514f
Johnathan Taylor Sutton <jtsutto@mac09-cs.ms-2345.okstate.edu>
2016-11-22 19:40:58 -0600

    fixed error. Need to rebind all my buttons

--- Chefulate/Chefulate/NewRecipeViewController.swift
+++ Chefulate/Chefulate/NewRecipeViewController.swift
@@ -1,84 +1,159 @@
 //
-//  RecipeInstructionsViewController.swift
+//  NewRecipeViewController.swift
 //  Chefulate
 //
-//  Created by Mahesh Kumar Kankipati on 11/19/16.
+//  Created by Mahesh Kumar Kankipati on 11/14/16.
 //  Copyright © 2016 Johnathan Taylor Sutton. All rights reserved.
 //
 
 import UIKit
+import Foundation
 
-class RecipeInstructionsViewController: UIViewController,UITextViewDelegate {
-    
-    @IBOutlet var instructionLabel: UILabel!
-    @IBOutlet weak var instruction1: UITextView!
-    @IBOutlet var InstructionBox: UITextView!
-    
-    @IBOutlet weak var closebutton: UIButton!
-    @IBOutlet weak var savebutton: UIButton!
-    var Sequence: Int = Int()
-    var R_ID: Int = Int()
-    
-    
+class NewRecipeViewController: UIViewController{
+    @IBOutlet weak var RecipeTitle: UITextField!
+    @IBOutlet weak var servingSize: UITextField!
     var radius : Int = Int()
     
-    @IBAction func saveInstructions(_ sender: AnyObject) {
-        let data = self.InstructionBox.text
-        let alertControl = UIAlertController(title: "Ready to save?", message: "", preferredStyle: .alert)
-        
-        let done = UIAlertAction(title: "Yes", style: .destructive) { (_) in
-            self.addInstructionData(Data:data!)
-        }
-        
-        let cancel = UIAlertAction(title: "No", style: .destructive) { (_) in
-        }
-        
-        alertControl.addAction(done)
-        alertControl.addAction(cancel)
-        self.present(alertControl, animated: true, completion: nil)
-        
-        
-    }
+    var UID: Int = 0
+    var U_Full: String = ""
+    var R_ID: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("R_ID:\(R_ID) Sequence: \(Sequence)")
-        instructionLabel.text! = "Instruction:"
-        instruction1.delegate = self
-        instruction1.backgroundColor = UIColor(white: 1, alpha: 0.5)
-        radius = 15
-        savebutton.layer.cornerRadius = CGFloat(radius)
-        
-        // Do any additional setup after loading the view.
     }
     
-    func addInstructionData(Data:String){
-        print(Data)
-        
+    @IBAction func unwindToNewRecipe(segue: UIStoryboardSegue){
+        print("Test un")
+        deleteRecipe()
     }
+    @IBAction func unwindToNewRecipeFromInstView(segue: UIStoryboardSegue){}
     
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "backToAddIngredientUnwindWithSegue"{
-            let vc = segue.destination as! NewInstructionViewController
-            //vc.ins_data.append(Instruction)
+    @IBAction func submitRecipe(_ sender: AnyObject) {
+        let alertControl = UIAlertController(title: "Are you sure?", message: "", preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "Cancel", style: .destructive){
+            (result : UIAlertAction) in debugPrint("Add")
         }
+        let save = UIAlertAction(title: "Save", style: .destructive) { (_) -> Void in
+            self.addRecipeData()
+        }
+        alertControl.addAction(save)
+        alertControl.addAction(cancel)
+        self.present(alertControl, animated: true, completion: nil)
     }
     
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
-        instruction1.resignFirstResponder()
+    func addRecipeData(){
+        let S_Size = servingSize.text!
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        let date_formatted = formatter.string(from: date)
+        let urlString = "https://cs.okstate.edu/~jtsutto/services.php/5/\(R_ID)/\(U_Full)/\(UID)/\(date_formatted)/\(S_Size)"
+        let urlString_Fixed = urlString.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.urlQueryAllowed)
+        let url = URL(string: urlString_Fixed! )!
+        print("URL: \(url)")
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
         
-        return true
+        let task = session.dataTask(with: url){(data,response,error)in
+            guard error == nil else{
+                print("Error in session call: \(error)")
+                return
+            }
+            guard let result = data else {
+                print("No data reveived")
+                return
+            }
+            do {
+                let json = try JSONSerialization.jsonObject(with: result, options: .allowFragments) as? NSDictionary
+                print("JSON test data returned : \(json)")
+                DispatchQueue.main.async{
+                    self.getID()
+                }
+            }catch {
+                print("Error Serializing JSON data : \(error)")
+            }
+            
+            
+        }
+        task.resume()
     }
+    
+    func getID(){
+        let urlString = "https://cs.okstate.edu/~jtsutto/services.php/18/\(UID)"
+        let urlString_Fixed = urlString.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.urlQueryAllowed)
+        let url = URL(string: urlString_Fixed! )!
+        print("URL: \(url)")
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        
+        let task = session.dataTask(with: url){(data,response,error)in
+            guard error == nil else{
+                print("Error in session call: \(error)")
+                return
+            }
+            guard let result = data else {
+                print("No data reveived")
+                return
+            }
+            do {
+                let json = try JSONSerialization.jsonObject(with: result, options: .allowFragments) as? NSDictionary
+                print("JSON test data returned : \(json)")
+                self.R_ID = (Int)(json!["Recipie_ID"] as! String)!
+            }catch {
+                print("Error Serializing JSON data : \(error)")
+            }
+            DispatchQueue.main.async {
+                let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+                let vc = mainStoryboard.instantiateViewController(withIdentifier: "NewIng") as! NewIngredientViewController
+                vc.R_ID = self.R_ID
+                self.present(vc, animated: true, completion: nil)
+                
+            }
+        }
+        task.resume()
+    }
+    
+    func deleteRecipe(){
+        let urlString = "https://cs.okstate.edu/~jtsutto/services.php/9/\(R_ID)"
+        let urlString_Fixed = urlString.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.urlQueryAllowed)
+        let url = URL(string: urlString_Fixed! )!
+        print("URL: \(url)")
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        
+        let task = session.dataTask(with: url){(data,response,error)in
+            guard error == nil else{
+                print("Error in session call: \(error)")
+                return
+            }
+            guard let result = data else {
+                print("No data reveived")
+                return
+            }
+            do {
+                let json = try JSONSerialization.jsonObject(with: result, options: .allowFragments) as? NSDictionary
+                print("JSON delete data returned : \(json)")
+            }catch {
+                print("Error Serializing JSON data : \(error)")
+            }
+        }
+        task.resume()
+    }
+    
+    
+    
     
     func hideKeyboardWhenTappedAround() {
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(RecipeInstructionsViewController.dismissKeyboard))
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(NewRecipeViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
     }
     
     func dismissKeyboard() {
         view.endEditing(true)
     }
+    
     
     /*
      // MARK: - Navigation
